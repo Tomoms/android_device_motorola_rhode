74b6345f4a2958bcbce4f96f6a4b247f515e4b09
Tommaso Fonda <fondatommaso2@gmail.com>
2023-07-22 08:53:57 +0200

    rhode: Set my custom build ID
    Change-Id: Iedadbb073c4f9a4eac4fc30df270ba80cfc18c77

--- lineage_rhode.mk
+++ lineage_rhode.mk
@@ -13,6 +13,8 @@ $(call inherit-product, device/motorola/rhode/device.mk)
 # Inherit some common LineageOS stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+TARGET_UNOFFICIAL_BUILD_ID := Tom
+
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := lineage_rhode
 PRODUCT_DEVICE := rhode
