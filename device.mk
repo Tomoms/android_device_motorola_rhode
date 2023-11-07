0bf66cb5f73047c4d83bc038d16133e7e230c8e6
Tommaso Fonda <fondatommaso2@gmail.com>
2023-11-07 18:21:06 +0100

    Revert "DO NOT PICK: nfc experiments"
    This reverts commit da76309705c284e567c1e33e6763f7db796da868.

--- device.mk
+++ device.mk
@@ -31,16 +31,11 @@ PRODUCT_PACKAGES += \
     F-DroidPrivilegedExtension
 
 # NFC
-PRODUCT_SOONG_NAMESPACES += \
-    vendor/nxp/nfc \
-    vendor/nxp/secure_element
-
 PRODUCT_PACKAGES += \
-    android.hardware.nfc@1.2-service.pn8x
+    android.hardware.nfc@1.2-service
 
 PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/com.nxp.mifare.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.nxp.mifare.xml \
-    vendor/nxp/nfc/halimpl/libnfc-nxp_RF-PN553_example.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp_RF.conf
+    frameworks/native/data/etc/com.nxp.mifare.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.nxp.mifare.xml
 
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
@@ -52,9 +47,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.nfc.hce.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.hce.xml \
     frameworks/native/data/etc/android.hardware.nfc.hcef.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.hcef.xml \
     frameworks/native/data/etc/android.hardware.nfc.uicc.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.uicc.xml \
-    frameworks/native/data/etc/android.hardware.nfc.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.xml \
-    frameworks/native/data/etc/android.hardware.se.omapi.uicc.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.se.omapi.uicc.xml \
-    frameworks/native/data/etc/com.android.nfc_extras.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.android.nfc_extras.xml
+    frameworks/native/data/etc/android.hardware.nfc.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.xml
 
 # Rootdir
 PRODUCT_PACKAGES += \
